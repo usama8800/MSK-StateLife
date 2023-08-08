9ad18d7762c0f702eacc736ba5f258f9af0edc5c
Usama Ahsan <usama8800@gmail.com>
2023-08-08 17:39:38 +0500

    ignore empty folders

--- src/index.ts
+++ src/index.ts
@@ -192,10 +192,7 @@ async function getPatients() {
       } else if (stat.isDirectory()) {
         const mergedPdf = await PDFLib.PDFDocument.create();
         const filenames = await fsP.readdir(pathname);
-        if (filenames.length === 0) {
-          log(`${patientFolder} has an empty folder '${name}'`);
-          continue patientLoop;
-        }
+        if (filenames.length === 0) continue patientLoop;
         for (let i = 0; i < filenames.length; i++) {
           const filepath = path.resolve(pathname, filenames[i]);
           stat = await fsP.stat(filepath);
